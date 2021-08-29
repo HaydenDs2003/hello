089dfbcb175e0d4cdc0ec0f78b5d53158729a61d
Hayden <hdavies3@huskers.unl.edu>
2021-08-29 20:22:20 +0000

    added my major to output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Hayden Davies
+ *         hdavies3@huskers.unl.edu
+ * Date: 2020/08/26cd
+ * Major: Computer Engineering
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World, I'm Hayden!!\n");
+  printf("My Major is Computer Engineering\n")
+
+  return 0;
+}
