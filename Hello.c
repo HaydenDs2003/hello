a32785ab884908b72acf77ac87a8583f69a4fa4d
Hayden <hdavies3@huskers.unl.edu>
2021-08-29 18:33:47 +0000

    initial commit

---
+++ Hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Hayden Davies
+ *         hdavies3@huskers.unl.edu
+ * Date: 2020/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hayden\n");
+
+  return 0;
+}
